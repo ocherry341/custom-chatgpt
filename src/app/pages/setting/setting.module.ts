7367919e0043c17aa4eed33f04e369a2607c4bca
ocherry341 <91065936+ocherry341@users.noreply.github.com>
2023-03-06 21:23:38 +0800

    update

--- src/app/pages/setting/setting.module.ts
+++ src/app/pages/setting/setting.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
+import { InputNumberModule, SelectModule, SliderModule, TabsModule } from 'ng-devui';
 import { SharedModule } from 'src/app/@shared/shared.module';
 import { SettingComponent } from './setting.component';
 
 @NgModule({
   imports: [
-    SharedModule
+    SharedModule,
+    SliderModule,
+    InputNumberModule,
+    SelectModule,
+    TabsModule
   ],
   declarations: [SettingComponent],
   exports: [SettingComponent]
